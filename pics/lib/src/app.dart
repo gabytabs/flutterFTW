79cf6017e0461a29f54ec174bfb7862224f482fc
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2018-08-12 09:18:54 -0400

    added s2-c1

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-// todo: create a class that will be the custom widget, this class must extend the `stateless` base class
+// todo: create a class that will be the custom widget,
+// this class must extend the `stateless` base class
+class App extends StatelessWidget {
 
+}
 
 // todo: define a `build widget` that this widget will show
