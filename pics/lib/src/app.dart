7c9d547649a151f3da558a6570bdc48186440272
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2018-08-12 11:21:29 -0400

    added s2-c4

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
+          body: Text('$counter'),
           appBar: AppBar(
               title: Text('Lets see some images!')
           ),
           floatingActionButton: FloatingActionButton(
               child: Icon(Icons.add_a_photo),
               onPressed: () {
-                print('Hey there');
+                setState(() => counter += 1 );
               }
           ),
         )
